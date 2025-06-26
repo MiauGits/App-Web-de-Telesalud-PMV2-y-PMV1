9c4a96f6baaf9868f18d45d445dab4aaaf304745
MiauGits <143139622+MiauGits@users.noreply.github.com>
2025-06-25 21:50:19 -0500

    Update footer.php
    changed footer.php to add chatbot ai

--- telesalud-p-q-r8/includes/templates/footer.php
+++ telesalud-p-q-r8/includes/templates/footer.php
@@ -4,5 +4,21 @@
     </div>
   </footer>
   <script src="/js/app.js"></script>
+<script type="text/javascript">
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '685b6cf985cda19294f9d1d4' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
 </body>
 </html>
